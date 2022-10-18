ea9c1be48b3dca63c507389bfb9eef33dbdd4d72
takahiro.tominaga <tt.300607@gmail.com>
2022-10-18 21:45:09 +0900

    initial commit

---
+++ SecureSwiftUI/SecureSwiftUI/SecureSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SecureSwiftUIApp.swift
+//  SecureSwiftUI
+//
+//  Created by Takahiro Tominaga on 2022/10/18.
+//
+
+import SwiftUI
+
+@main
+struct SecureSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
